a50184ff512ffd2a4fdbbb52565b1663772fb342
Jivesh21 <jiveshsharma2220@gmail.com>
2026-07-30 19:53:02 +0530

    connection with db

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// require('dotenv').config()
+import connectDB from "./db/index.js";
+import dotenv from 'dotenv'
+
+dotenv.config({ 
+    path:'./env'
+})
+    
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
