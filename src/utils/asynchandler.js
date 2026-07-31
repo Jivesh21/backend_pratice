43dd1f01d6ee245daae79920e6d2442fe2745f5a
Jivesh21 <jiveshsharma2220@gmail.com>
2026-07-31 18:58:37 +0530

    utils

--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -3,13 +3,23 @@
 // const asyncHandler = (func) => async () =>{}
 
 
-    const asyncHandler = (fn) => async(req,resizeBy,next) =>{
-        try{
-            await fn(req,res,next)
-        }catch(error){
-            res.status(error.code||500).json({
-                success: false,
-                message : error.message
-            })
-        }
+    // const asyncHandler = (fn) => async(req,resizeBy,next) =>{
+    //     try{
+    //         await fn(req,res,next)
+    //     }catch(error){
+    //         res.status(error.code||500).json({
+    //             success: false,
+    //             message : error.message
+    //         })
+    //     }
+    // }
+
+
+const asyncHandler = (requestHandler) =>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).
+        catch((error)=>next(error))
     }
+}
+
+export {asyncHandler}
